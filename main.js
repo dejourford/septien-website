e2b9f214102a1f59f63844f4c7b914c824274661
De'Jour Ford <dejourford@my.unt.edu>
2023-03-24 00:50:12 -0500

    add hero cycle through words js effect

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const closeBtn = document.querySelector('.fa-xmark')
 const openBtn =  document.querySelector('.fa-bars-staggered')
 const mobileNav = document.querySelector('.mobile-nav')
 
-
 closeMenu = () => {
     mobileNav.classList.remove('show')
     mobileNav.classList.add('close')
@@ -12,4 +11,23 @@ closeMenu = () => {
 showMenu = () => {
     mobileNav.classList.remove('close')
     mobileNav.classList.add('show')
+}
+
+// HERO TEXT EFFECT
+var words = ['SONG PRODUCTION', 'VIDEO', 'SOCIAL MEDIA', 'PROMOTION',
+'TRAINING', 'SUMMER PROGRAMS', 'ACTING'];
+
+textSequence(0);
+function textSequence(i) {
+
+    if (words.length > i) {
+        setTimeout(function() {
+            document.getElementById("hero-text").innerHTML = words[i];
+            textSequence(++i);
+        }, 2000); // 1 second (in milliseconds)
+
+    } else if (words.length == i) { // Loop
+        textSequence(0);
+    }
+
 }
